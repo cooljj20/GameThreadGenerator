fc1d4a82683cdb12b0257f14539af307582c596f
Jimmy Garcia <cooljj_20@sbcglobal.net>
2014-01-28 22:44:06 -0600

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,452 @@
+time = Time.new
+
+class Bobcats
+      def team
+        "Charlotte Bobcats"
+      end
+      def subreddit
+        "CharlotteBobcats"
+      end
+      def shortcode
+        "CHA"
+      end
+end
+class Bucks
+      def team
+        "Milwaukee Bucks"
+      end
+      def subreddit
+        "mkebucks"
+      end
+      def shortcode
+        "MIL"
+      end
+end
+class Bulls
+      def team
+        "Chicago Bulls"
+      end
+      def subreddit
+        "chicagobulls"
+      end
+      def shortcode
+        "CHI"
+      end
+end
+class Cavaliers
+      def team
+        "Cleveland Cavaliers"
+      end
+      def subreddit
+        "clevelandcavs"
+      end
+      def shortcode
+        "CLE"
+      end
+end
+class Celtics
+      def team
+        "Boston Celtics"
+      end
+      def subreddit
+        "bostonceltics"
+      end
+      def shortcode
+        "BOS"
+      end
+end
+class Clippers
+      def team
+        "Los Angeles Clippers"
+      end
+      def subreddit
+        "LAClippers"
+      end
+      def shortcode
+        "LAC"
+      end
+end
+class Grizzlies
+      def team
+        "Memphis Grizzlies"
+      end
+      def subreddit
+        "memphisgrizzlies"
+      end
+      def shortcode
+        "MEM"
+      end
+end
+class Hawks
+      def team
+        "Atlanta Hawks"
+      end
+      def subreddit
+        "AtlantaHawks"
+      end
+      def shortcode
+        "ATL"
+      end
+end
+class Heat
+      def team
+        "Miami Heat"
+      end
+      def subreddit
+        "heat"
+      end
+      def shortcode
+        "MIA"
+      end
+end
+class Jazz
+      def team
+        "Utah Jazz"
+      end
+      def subreddit
+        "UtahJazz"
+      end
+      def shortcode
+        "UTA"
+      end
+end
+class Kings
+      def team
+        "Sacramento Kings"
+      end
+      def subreddit
+        "kings"
+      end
+      def shortcode
+        "SAC"
+      end
+end
+class Knicks
+      def team
+        "New York Knicks"
+      end
+      def subreddit
+        "NYKnicks"
+      end
+      def shortcode
+        "NYK"
+      end
+end
+class Lakers
+      def team
+        "Los Angeles Lakers"
+      end
+      def subreddit
+        "lakers"
+      end
+      def shortcode
+        "LAL"
+      end
+end
+class Magic
+      def team
+        "Orlando Magic"
+      end
+      def subreddit
+        "orlandomagic"
+      end
+      def shortcode
+        "ORL"
+      end
+end
+class Mavericks
+      def team
+        "Dallas Maverics"
+      end
+      def subreddit
+        "Mavericks"
+      end
+      def shortcode
+        "DAL"
+      end
+end
+class Nets
+      def team
+        "Brooklyn Nets"
+      end
+      def subreddit
+        "GoNets"
+      end
+      def shortcode
+        "BKN"
+      end
+end
+class Nuggets
+      def team
+        "Denver Nuggets"
+      end
+      def subreddit
+        "denvernuggets"
+      end
+      def shortcode
+        "DEN"
+      end
+end
+class Pacers
+      def team
+        "Indiana Pacers"
+      end
+      def subreddit
+        "IndianaPacers"
+      end
+      def shortcode
+        "IND"
+      end
+end
+class Pelicans
+      def team
+        "New Orleans Pelicans"
+      end
+      def subreddit
+        "NOLAPelicans"
+      end
+      def shortcode
+        "NOP"
+      end
+end
+class Pistons
+      def team
+        "Detroit Pistons"
+      end
+      def subreddit
+        "DetroitPistons"
+      end
+      def shortcode
+        "DET"
+      end
+end
+class Raptors
+      def team
+        "Toronto Raptors"
+      end
+      def subreddit
+        "torontoraptors"
+      end
+      def shortcode
+        "TOR"
+      end
+end
+class Rockets
+      def team
+        "Houston Rockets"
+      end
+      def subreddit
+        "rockets"
+      end
+      def shortcode
+        "HOU"
+      end
+end
+class Sixers
+      def team
+        "Philadelphia 76ers"
+      end
+      def subreddit
+        "sixers"
+      end
+      def shortcode
+        "PHI"
+      end
+end
+class Spurs
+      def team
+        "San Antonio Spurs"
+      end
+      def subreddit
+        "NBASpurs"
+      end
+      def shortcode
+        "SAS"
+      end
+end
+class Suns
+      def team
+        "Phoenix Suns"
+      end
+      def subreddit
+        "SUNS"
+      end
+      def shortcode
+        "PHX"
+      end
+end
+class Thunder
+      def team
+        "Oklahoma City Thunder"
+      end
+      def subreddit
+        "Thunder"
+      end
+      def shortcode
+        "OKC"
+      end
+end
+class Trailblazers
+      def team
+        "Portland Trail Blazers"
+      end
+      def subreddit
+        "ripcity"
+      end
+      def shortcode
+        "POR"
+      end
+end
+class Timberwolves
+      def team
+        "Minnesota Timberwolves"
+      end
+      def subreddit
+        "timberwolves"
+      end
+      def shortcode
+        "MIN"
+      end
+end
+class Warriors
+      def team
+        "Golden State Warriors"
+      end
+      def subreddit
+        "warriors"
+      end
+      def shortcode
+        "GSW"
+      end
+end
+class Wizards
+      def team
+        "Washington Wizards"
+      end
+      def subreddit
+        "washingtonwizards"
+      end
+      def shortcode
+        "WAS"
+      end
+end
+
+opponent = "bobcats"
+
+case opponent
+when "bobcats"
+  bobcats = Bobcats.new
+when "bucks"
+  bucks = Bucks.new
+when "bulls"
+  bulls = Bull.new
+when "cavaliers"
+  cavaliers = Cavaliers.new
+when "celtics"
+  celtics = Celtics.new
+when "clippers"
+  clippers = Clippers.new
+when "grizzlies"
+  grizzlies = Grizzlies.new
+when "hawks"
+  hawks = Hawks.new
+when "heat"
+  heat = Heat.new
+when "jazz"
+  jazz = Jazz.new
+when "kings"
+  kings = Kings.new
+when "knicks"
+  knicks = Knicks.new
+when "lakers"
+  lakers = Lakers.new
+when "magic"
+  magic = Magic.new
+when "mavericks"
+  mavericks = Mavericks.new
+when "nets"
+  nets = Nets.new
+when "nuggets"
+  nuggets = Nuggets.new
+when "pacers"
+  pacers = Pacers.new
+when "pelicans"
+  pelicans = Pelicans.new
+when "pistons"
+  pistons = Pistons.new
+when "raptors"
+  raptors = Raptors.new
+when "rockets"
+  rockets1 = Rockets.new
+when "sixers"
+  sixers = Sixers.new
+when "spurs"
+  spurs = Spurs.new
+when "suns"
+  suns = Suns.new
+when "thunder"
+  thunder = Thunder.new
+when "trailblazers"
+  trailblazers = Trailblazer.new
+when "timberwolves"
+  timberolves = Timberwolves.new
+when "warriors"
+  warriors = Warriors.new
+when "wizards"
+  wizards = Wizards.new
+else
+  puts "Something went wrong. um yea...... you broke it"
+end
+
+rockets = Rockets.new
+
+
+homeGame = true
+
+curYear = time.year.to_s
+curMonth = time.month
+curDay = time.day
+
+
+if time.month < 10
+  curMonth = time.month.to_s
+  curMonth = "0" + curMonth
+end
+if time.day < 10
+  curDay = time.day.to_s
+  curDay = "0" + curDay
+end
+
+curMonth = curMonth.to_s
+curDay = curDay.to_s
+
+oppSub = "NBASpurs"
+
+if homeGame              
+  puts "##General Information"
+  puts "**TIME**     |**MEDIA**                            |**LOCATION**        |**MISC**"
+  puts ":------------|:------------------------------------|:-------------------|:-------------------------"
+  puts "08:00 Eastern |**TV**: National: NBATV, Away: FOX Sports Southwest+, Home: CSN-Houston                       | Toyota Center, Houston, TX               | [Live chat](http://webchat.freenode.net/?channels=r/NBA&uio=MTE9MjQ255/)"
+  puts "07:00 Central |**Streaming**: N/A | **Team Subreddits**|"
+  puts "06:00 Mountain|**Game Story**: [NBA.com](http://www.nba.com/games/" + curYear + curMonth + curDay + "/SASHOU/gameinfo.html#nbaGIlive)| [/r/"+ oppSub +"](http://reddit.com/r/"+ oppSub +")          |"
+  puts "05:00 Pacific |**Box Score**: [NBA.com](http://www.nba.com/games/" + curYear + curMonth + curDay + "/SASHOU/gameinfo.html#nbaGIboxscore) | [/r/rockets](http://reddit.com/r/rockets)          |"
+  puts "Last 10|**Rockets**: |**opponent**:"
+  puts "-----"
+  puts "[Reddit Stream](http://nba-gamethread.herokuapp.com/reddit-stream/) (You must click this link from the comment page.)"
+else
+  puts "false"
+  puts "##General Information"
+  puts "**TIME**     |**MEDIA**                            |**LOCATION**        |**MISC**"
+  puts ":------------|:------------------------------------|:-------------------|:-------------------------"
+  puts "08:00 Eastern |**TV**: National: NBATV, Away: FOX Sports Southwest+, Home: CSN-Houston                       | Toyota Center, Houston, TX               | [Live chat](http://webchat.freenode.net/?channels=r/NBA&uio=MTE9MjQ255/)"
+  puts "07:00 Central |**Streaming**: N/A | **Team Subreddits**|"
+  puts "06:00 Mountain|**Game Story**: [NBA.com](http://www.nba.com/games/" + curYear + curMonth + curDay + "/SASHOU/gameinfo.html#nbaGIlive)| [/r/"+ oppSub +"](http://reddit.com/r/"+ oppSub +")          |"
+  puts "05:00 Pacific |**Box Score**: [NBA.com](http://www.nba.com/games/" + curYear + curMonth + curDay + "/SASHOU/gameinfo.html#nbaGIboxscore) | [/r/rockets](http://reddit.com/r/rockets)          |"
+  puts "Last 10|**Rockets**: |**opponent**:"
+  puts "-----"
+  puts "[Reddit Stream](http://nba-gamethread.herokuapp.com/reddit-stream/) (You must click this link from the comment page.)"
+end
+
+
+
+
